648581e4688cc4f11b76421c42d40c7dc56a3a35
Jim Katz <jimk@tbrc.org>
2020-09-18 11:46:32 -0400

    volume-manifest-builder-34 Support local FS
    Code complete

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ from setuptools import setup, find_packages
 
 console_scripts = ['manifestforwork = v_m_b.manifestBuilder:manifestShell',
                    'manifestFromS3 = v_m_b.manifestBuilder:manifestFromS3']
-setup(name='bdrc-volume-manifest-builder', version='1.0.3', packages=find_packages(),
+setup(name='bdrc-volume-manifest-builder', version='1.0.4', packages=find_packages(),
       url='https://github.com/buda-base/volume-manifest-builder/', license='', author='jimk',
       author_email='jimk@tbrc.org',
-      description='Creates manifests for syncd works.', entry_points={'console_scripts': console_scripts},
-      install_requires=['boto3', 'requests', 'lxml', 'pillow', 's3transfer', 'botocore'], python_requires='>=3.7',
+      description='Creates manifests for syncd works.',
+      entry_points={'console_scripts': console_scripts},
+      install_requires=['boto3', 'requests', 'lxml', 'pillow', 's3transfer', 'botocore', 'bdrc_DBAppParser', 'aiofiles', 'requests'],
+      python_requires='>=3.7',
       classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
-                   "Operating System :: MacOS :: MacOS X", "Operating System :: OS Independent",
+                   "Operating System :: OS Independent",
                    "Development Status :: 5 - Production/Stable"])
